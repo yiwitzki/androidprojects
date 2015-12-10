48d56c7f663bd03441d9b96e3df56541bdea341d
yiwitzki <byebyehighbury@163.com>
2015-12-10 15:30:10 +0800

    add gitignore

--- robolectric-master/robolectric-master/src/main/java/com/xtremelabs/robolectric/shadows/ShadowViewAnimator.java
+++
@@ -1,8 +0,0 @@
-package com.xtremelabs.robolectric.shadows;
-
-import android.widget.ViewAnimator;
-import com.xtremelabs.robolectric.internal.Implements;
-
-@Implements(ViewAnimator.class)
-public class ShadowViewAnimator extends ShadowFrameLayout {
-}
